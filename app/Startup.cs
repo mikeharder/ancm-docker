8f47dbcba60aa6f1d0bc6e5333a7061d33d382ea
Mike Harder <mharder@microsoft.com>
2018-06-14 19:12:57 -0700

    Change app to print assembly name

--- app/Startup.cs
+++ app/Startup.cs
@@ -27,7 +27,7 @@ namespace app
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(typeof(IHostingEnvironment).Assembly.FullName);
             });
         }
     }
