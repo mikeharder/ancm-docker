20efda17ca43b6f789bed66a3b7803c4246a34b5
Mike Harder <mharder@microsoft.com>
2018-06-29 10:55:42 -0700

    Add more diagnostic output to app

--- 2.2/app/Startup.cs
+++ 2.2/app/Startup.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
 
 namespace app
 {
     public class Startup
     {
+        private static readonly string _info =
+$@"
+RuntimeInformation.OSDescription:            {RuntimeInformation.OSDescription}
+RuntimeInformation.OSArchitecture:           {RuntimeInformation.OSArchitecture}
+RuntimeInformation.ProcessArchitecture:      {RuntimeInformation.ProcessArchitecture}
+DependencyContext.Default.Target.Framework:  {DependencyContext.Default.Target.Framework}
+ASP.NET Version:                             {typeof(IHostingEnvironment).Assembly.GetName().Version}
+";
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +34,7 @@ namespace app
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync(typeof(IHostingEnvironment).Assembly.FullName);
+                await context.Response.WriteAsync(_info);
             });
         }
     }
