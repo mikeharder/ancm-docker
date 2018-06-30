da5ad699f9374a05f9c316c20725b476cfb2ba00
Mike Harder <mharder@microsoft.com>
2018-06-29 17:57:48 -0700

    Rename folder from "express" to "iis-express" to workaround .gitignore

--- app/2.2/inproc-express/build.cmd
+++ app/2.2/inproc-express/build.cmd
@@ -1,1 +1,1 @@
-docker build -t ancm-app:2.2-inproc-express --build-arg hosting_model=inprocess %~dp0/.. -f %~dp0/../express/Dockerfile
+docker build -t ancm-app:2.2-inproc-express --build-arg hosting_model=inprocess %~dp0/.. -f %~dp0/../iis-express/Dockerfile
--- app/2.2/outproc-express/build.cmd
+++ app/2.2/outproc-express/build.cmd
@@ -1,1 +1,1 @@
-docker build -t ancm-app:2.2-outproc-express --build-arg hosting_model=outofprocess %~dp0/.. -f %~dp0/../express/Dockerfile
+docker build -t ancm-app:2.2-outproc-express --build-arg hosting_model=outofprocess %~dp0/.. -f %~dp0/../iis-express/Dockerfile
