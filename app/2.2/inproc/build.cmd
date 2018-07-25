0a370a2feb29ce821bb84e4e300e8b81b715b459
Mike Harder <mharder@microsoft.com>
2018-07-25 16:57:02 -0700

    Move IIS dockerfile into "iis" folder

--- app/2.2/inproc/build.cmd
+++ app/2.2/inproc/build.cmd
@@ -1,1 +1,1 @@
-docker build -t ancm-app:2.2-inproc --build-arg hosting_model=inprocess %~dp0/.. %*
+docker build -t ancm-app:2.2-inproc --build-arg hosting_model=inprocess %~dp0/.. -f %~dp0/../iis/Dockerfile
--- app/2.2/outproc/build.cmd
+++ app/2.2/outproc/build.cmd
@@ -1,1 +1,1 @@
-docker build -t ancm-app:2.2-outproc --build-arg hosting_model=outofprocess %~dp0/.. %*
+docker build -t ancm-app:2.2-outproc --build-arg hosting_model=outofprocess %~dp0/.. -f %~dp0/../iis/Dockerfile
