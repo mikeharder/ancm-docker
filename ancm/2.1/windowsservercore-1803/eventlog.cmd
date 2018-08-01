0bdbb24d00984af24d504160b1c064da9bebec7f
Mike Harder <mharder@microsoft.com>
2018-08-01 10:06:36 -0700

    Remove extra whitespace

--- ancm/2.1/windowsservercore-1803/eventlog.cmd
+++ ancm/2.1/windowsservercore-1803/eventlog.cmd
@@ -1,3 +1,3 @@
 @call %~dp0\util\util.cmd
 
-docker exec -it %container% powershell "Get-WinEvent -ProviderName 'Microsoft-Windows-IIS*','IIS AspNetCore Module*'  | Format-List -Property Message,ProviderName,TimeCreated"
+docker exec -it %container% powershell "Get-WinEvent -ProviderName 'Microsoft-Windows-IIS*','IIS AspNetCore Module*' | Format-List -Property Message,ProviderName,TimeCreated"
