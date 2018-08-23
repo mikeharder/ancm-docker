08e7f1d560b85c422707db49b15c8e6f5041e4b4
Mike Harder <mharder@microsoft.com>
2018-08-23 14:56:43 -0700

    Fix call to common util.cmd

--- ancm/2.1.2/windowsservercore-1803/util/util.cmd
+++ ancm/2.1.2/windowsservercore-1803/util/util.cmd
@@ -1,4 +1,4 @@
-@%~dp0\..\..\..\common\util.cmd
+@call %~dp0\..\..\..\common\util.cmd
 
 @set image=microsoft/ancm:2.1.2-windowsservercore-1803
 @set container=microsoft-ancm-2.1.2-windowsservercore-1803
--- ancm/2.1.3/windowsservercore-1803/util/util.cmd
+++ ancm/2.1.3/windowsservercore-1803/util/util.cmd
@@ -1,4 +1,4 @@
-@%~dp0\..\..\..\common\util.cmd
+@call %~dp0\..\..\..\common\util.cmd
 
 @set image=microsoft/ancm:2.1.3-windowsservercore-1803
 @set container=microsoft-ancm-2.1.3-windowsservercore-1803
