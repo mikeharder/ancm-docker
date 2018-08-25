38643f1da62a1ceb67d889b7faf1a1218188e482
Mike Harder <mharder@microsoft.com>
2018-08-24 17:41:15 -0700

    Remove comments

--- app/2.1/outproc-express/publish.cmd
+++ app/2.1/outproc-express/publish.cmd
@@ -12,7 +12,6 @@ dotnet publish -c Release -o %ancm%\share\%publish_path% %~dp0..\app.csproj
 
 : Create folder for stdout logs and give IIS full permissions
 mkdir %ancm%\share\%publish_path%\logs
-: docker exec -it %container% cmd /c icacls C:\share\%publish_path%\logs /grant "IIS APPPOOL\DefaultAppPool":F
 
 : Register app with IIS
 docker exec -it %container% cmd /c %appcmd% add app /site.name:"WebSite1" /path:/%http_path% /physicalPath:C:\share\%publish_path%
